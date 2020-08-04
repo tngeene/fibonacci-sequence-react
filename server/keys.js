90edbf7ec749602c75237c896e9e405afdf9a6ed
ted <tngeene27@gmail.com>
2020-08-04 15:49:12 +0300

    initial commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    redisHost: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT,
+    pgUser: process.env.POSTGRES_USER,
+    pgHost: process.env.POSTGRES_HOST,
+    pgDatabase: process.env.POSTGRES_DATABASE,
+    pgPassword: process.env.POSTGRES_PASSWORD,
+    pgPort: process.env.PG_PORT
+};
